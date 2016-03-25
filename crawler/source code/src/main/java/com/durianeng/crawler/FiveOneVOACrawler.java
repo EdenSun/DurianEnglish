4872e9cd952d856a8e5db25e28bf72c069343f95
Eden Sun <durian.suen@gmail.com>
2016-03-25 10:25:35 +0800

    遍历分类页面，抓取每个分类页面中的所有分页，读取每个分页内的文章列表

--- crawler/source code/src/main/java/com/durianeng/crawler/FiveOneVOACrawler.java
+++ crawler/source code/src/main/java/com/durianeng/crawler/FiveOneVOACrawler.java
@@ -2,6 +2,7 @@ package com.durianeng.crawler;
 
 import com.durianeng.crawler.util.HttpHelper;
 
+
 import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,6 +10,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
@@ -36,14 +38,101 @@ public class FiveOneVOACrawler implements ICrawler {
             List<Category> categoryList = readCategory(document);
 
             for(Category category : categoryList ){
-                System.out.println(category);
+                logger.info("抓取分类：" + category);
             }
 
+            //TODO: 遍历分类，抓取每个分类页面的文章
+            readCategoryArticle( categoryList );
             //iterateAllArticle(categoryList);
 
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    private void readCategoryArticle(List<Category> categoryList) {
+        if (categoryList != null) {
+            for(Category category : categoryList ){
+                String categoryPageHtml = null;
+                try{
+                    logger.info("读取分类页面： " + category.url);
+                    categoryPageHtml = HttpHelper.getHtml(category.url);
+                }catch (Exception e){
+                    logger.error("读取URL出错,URL : "+ category.url);
+                    continue;
+                }
+
+                Document document = Jsoup.parse(categoryPageHtml,URL_HOME);
+                // 抓取分页，若有分页，遍历每个分页的Article，若没有分页，读取当前页的Ariticle
+                Element pageListElement = document.getElementById("pagelist");
+                if( pageListElement != null ){
+                    Elements pageListChildren = pageListElement.children();
+                    List<String> pageUrlList = new ArrayList<String>();
+                    for( Iterator<Element> it = pageListChildren.iterator() ;it.hasNext() ;){
+                        Element child = it.next();
+                        if( child.tagName().equals("a") ){
+                            String pageUrl = child.attr("abs:href");
+                            pageUrlList.add(pageUrl);
+                        }
+                    }
+                    logger.info("分页列表：" + pageUrlList);
+
+                    if( pageUrlList != null && pageUrlList.size() > 0 ){
+                        for(String pageUrl : pageUrlList ){
+                            logger.info("读取分页页面： " + pageUrl);
+                            String pageHtml = null;
+                            try {
+                                pageHtml = HttpHelper.getHtml(pageUrl);
+                            } catch (IOException e) {
+                                logger.error("读取页面HTML出错:" + pageUrl + ","+ e.getMessage());
+                            }
+                            document = Jsoup.parse(pageHtml,URL_HOME);
+                            List<Article> articleList = readArticleInDocument(document);
+
+                            if( category.articles == null ){
+                                category.articles = articleList;
+                            }else{
+                                category.articles.addAll(articleList);
+                            }
+                        }
+                    }
+                }else{  // pageListElement == null
+                    List<Article> articleList = readArticleInDocument(document);
+
+                    category.articles = articleList;
+                }
+
+            }
+        }
+    }
+
+    private List<Article> readArticleInDocument(Document document) {
+        List<Article> articleList = new ArrayList<Article>();
+
+        Element articleListElement = document.getElementById("list");
+        if( articleListElement != null ){
+            Elements liElementList = articleListElement.select("ul > li");
+
+            if( liElementList != null ){
+                for(Iterator<Element> it = liElementList.iterator() ; it.hasNext() ; ){
+                    /*<li>
+                        <img src="/images/lrc.gif">
+                        <img src="/images/yi.gif">
+                        <a href="/VOA_Special_English/apple-annouces-small-iphone-return-68679.html" target="_blank">Apple Announces the Return of a Smaller iPhone</a> (16-3-24)
+                    </li>*/
+                    Article article = new Article();
+                    Element liElement = it.next();
+                    Element aTag = liElement.getElementsByTag("a").get(0);
+                    String title = aTag.text();
+
+                    article.title = title;
+
+                    articleList.add(article);
+                    logger.info(article);
+                }
+            }
+        }
+        return articleList;
     }
 
     /**
@@ -64,7 +153,8 @@ public class FiveOneVOACrawler implements ICrawler {
             if( child.tagName().equals("div") && child.hasClass("left_nav_title")){
                 // 一级分类
                 //<div class="left_nav_title"><a href="/VOA_Special_English/">VOA慢速英语</a></div>
-                if( child.text().equals("友情链接") || child.text().equals("其它节目") ){
+                if( child.text().equals("友情链接") || child.text().equals("其它节目")
+                        || child.text().equals("VOA节目介绍")){
                     // 排除 友情链接 和 其它节目
                     jumpSection = true;
                     continue;
@@ -117,7 +207,7 @@ public class FiveOneVOACrawler implements ICrawler {
     }
 
     private void iterateAllArticle(String html){
-        Document document = Jsoup.parse(html);
+        Document document = Jsoup.parse(html,URL_HOME);
 
         // 读取页面文章列表
         //document.select();
@@ -131,6 +221,7 @@ public class FiveOneVOACrawler implements ICrawler {
         String categoryName;    // 分类名称
         String url;             // 分类页面URL
         Category parent;        // 父分类
+        List<Article> articles; //分类下的文章
 
         @Override
         public String toString() {
@@ -143,9 +234,20 @@ public class FiveOneVOACrawler implements ICrawler {
     }
 
     class Article{
-        String category;
         String title;
+        String content;
         List<String> tags;
         Date createDate;
+        String mp3FileUrl;
+        String lrcFileUrl;
+        String translateFileUrl;
+        List<String> articleImgUrlList;
+
+        @Override
+        public String toString() {
+            return "Article{" +
+                    "title='" + title + '\'' +
+                    '}';
+        }
     }
 }
